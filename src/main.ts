4633314b60e347f1abd0befb6442431fb71802b8
mooujarrar <mohyiddine1997@gmail.com>
2023-12-16 16:14:16 +0100

    enh#Add swagger to test the api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // SWAGGER FOR TESTING THE API
+  const config = new DocumentBuilder()
+    .setTitle('Gateway API')
+    .setDescription('This API is the gateway to the microservices')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
