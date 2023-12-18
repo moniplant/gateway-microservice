f075fba30bbeb72404830e41b29864365e79503f
mooujarrar <mohyiddine1997@gmail.com>
2023-12-18 12:30:27 +0100

    feat# register gateway as client to both Kafka and MQTT microservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import {
   PLANTS_CLIENT_ID,
   PLANTS_CONSUMER_GROUP_ID,
   PLANTS_SERVICE,
+  SENSORS_CLIENT_ID,
+  SENSORS_CONSUMER_GROUP_ID,
+  SENSORS_DATA_SERVICE,
+  SENSORS_SERVICE,
 } from './tokens';
 
 @Module({
   imports: [
+    // Microservice #1: CRUD on plants
     ClientsModule.register([
       {
         name: PLANTS_SERVICE,
@@ -22,6 +27,32 @@ import {
           consumer: {
             groupId: PLANTS_CONSUMER_GROUP_ID,
           },
+        },
+      },
+    ]),
+    // Microservice #2: CRUD on sensors
+    ClientsModule.register([
+      {
+        name: SENSORS_SERVICE,
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: SENSORS_CLIENT_ID,
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: SENSORS_CONSUMER_GROUP_ID,
+          },
+        },
+      },
+    ]),
+    // Microservice #3: Persist sensor data (Humidity, Temperature, Soil Moisture in a NoSQL DB)
+    ClientsModule.register([
+      {
+        name: SENSORS_DATA_SERVICE,
+        transport: Transport.MQTT,
+        options: {
+          url: 'mqtt://localhost:1883',
         },
       },
     ]),
--- src/tokens.ts
+++ src/tokens.ts
@@ -1,3 +1,8 @@
 export const PLANTS_SERVICE = 'PLANTS_SERVICE';
 export const PLANTS_CLIENT_ID = 'plant';
 export const PLANTS_CONSUMER_GROUP_ID = 'plant-consumer';
+
+export const SENSORS_SERVICE = 'SENSORS_DATA_SERVICE';
+export const SENSORS_DATA_SERVICE = 'SENSORS_DATA_SERVICE';
+export const SENSORS_CLIENT_ID = 'sensor';
+export const SENSORS_CONSUMER_GROUP_ID = 'sensor-consumer';
