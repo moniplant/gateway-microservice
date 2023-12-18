3979243713082a839a98511e9b253cf17bafc172
mooujarrar <mohyiddine1997@gmail.com>
2023-12-18 12:31:13 +0100

    refactor#extend events sent to Sensor and Plants microservices

--- src/events.ts
+++ src/events.ts
@@ -1,1 +1,10 @@
 export const CREATE_PLANT = 'plant.create';
+export const DELETE_PLANT = 'plant.delete';
+export const UPDATE_PLANT = 'plant.delete';
+export const RETRIEVE_PLANT = 'plant.retrieve';
+
+export const CREATE_SENSOR = 'sensor.create';
+export const DELETE_SENSOR = 'sensor.delete';
+export const UPDATE_SENSOR = 'sensor.delete';
+export const RETRIEVE_SENSOR = 'sensor.retrieve';
+export const SAVE_SENSOR_DATA = 'sensor.data.save';
