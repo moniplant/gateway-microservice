24750b6b0296f5d766d6d7f8a6290ebef3769c84
mooujarrar <mohyiddine1997@gmail.com>
2025-02-09 21:59:28 +0100

    docs; Add swagger documentation for sensor controller too

--- src/sensor/controller/sensor.controller.ts
+++ src/sensor/controller/sensor.controller.ts
@@ -11,7 +11,7 @@ import { SensorService } from '../service/sensor.service';
 import { CreateSensorDto } from '../dto/create-sensor.dto';
 import { UpdateSensorDto } from '../dto/update-sensor.dto';
 import { SensorDataDto } from '../dto/sensor-data.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 /**
  * SensorController handles HTTP requests related to sensors.
@@ -30,6 +30,7 @@ export class SensorController {
    * @param createSensorDto - Data Transfer Object containing sensor creation details.
    */
   @Post('')
+  @ApiOperation({ summary: 'Create new sensor and associate it to a plant.' })
   create(@Body() createSensorDto: CreateSensorDto) {
     this.sensorService.create(createSensorDto);
   }
@@ -40,6 +41,9 @@ export class SensorController {
    * @returns A promise that resolves to the list of sensors.
    */
   @Get(':plantId')
+  @ApiOperation({
+    summary: 'Retrieves sensors associated with a specific plant.',
+  })
   findPlantSensors(@Param('plantId') id: string) {
     return this.sensorService.findPlantSensors(id);
   }
@@ -50,6 +54,9 @@ export class SensorController {
    * @returns A promise that resolves to the sensor details.
    */
   @Get(':id')
+  @ApiOperation({
+    summary: 'Retrieves a specific sensor by its ID.',
+  })
   findSensor(@Param('id') id: string) {
     return this.sensorService.findOne(id);
   }
@@ -60,6 +67,9 @@ export class SensorController {
    * @param updateSensorDto - Data Transfer Object containing the updated sensor details.
    */
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Updates an existing sensor.',
+  })
   update(@Param('id') id: string, @Body() updateSensorDto: UpdateSensorDto) {
     this.sensorService.update(id, updateSensorDto);
   }
@@ -69,6 +79,9 @@ export class SensorController {
    * @param id - The ID of the sensor to be removed.
    */
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Removes a specific sensor by its ID.',
+  })
   removeSensor(@Param('id') id: string) {
     this.sensorService.remove(id);
   }
@@ -78,6 +91,9 @@ export class SensorController {
    * @param plantId - The ID of the plant whose sensors are to be removed.
    */
   @Delete(':plantId')
+  @ApiOperation({
+    summary: 'Removes all sensors associated with a specific plant.',
+  })
   removeAllPlantSensors(@Param('plantId') id: string) {
     this.sensorService.removePlantSensors(id);
   }
@@ -87,6 +103,10 @@ export class SensorController {
    * @param dataEntry - Data Transfer Object containing the sensor data to be saved.
    */
   @Post('sensorData')
+  @ApiOperation({
+    summary:
+      'Delegates the saving of sensor data to the sensor service. And then to the Sensor microservice.',
+  })
   saveSensorData(@Body() dataEntry: SensorDataDto) {
     this.sensorService.saveSensorEntry(dataEntry);
   }
