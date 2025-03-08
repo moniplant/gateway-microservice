0ba3be8be2b57f782ffa334365acbad73594938e
mooujarrar <mohyiddine1997@gmail.com>
2025-03-08 13:57:24 +0100

    bug# docker-compose command not known

--- service-runner.sh
+++ service-runner.sh
@@ -3,8 +3,8 @@
 ARCH=$(uname -m)
 if [ "$ARCH" = "armv7l" ]; then
   echo "Detected architecture: armv7l (ARM). Using armhf-specific override file."
-  docker-compose -f docker-compose.yml -f docker-compose.override.armhf.yml up
+  docker compose -f docker-compose.yml -f docker-compose.override.armhf.yml up
 else
   echo "Detected architecture: $ARCH. Using default configuration."
-  docker-compose up
+  docker compose up
 fi
