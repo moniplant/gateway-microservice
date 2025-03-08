17566448f70f72d2fcbebec2dc9362899742955c
mooujarrar <mohyiddine1997@gmail.com>
2025-03-08 14:46:43 +0100

    enh# run docker compose in detach mode

--- service-runner.sh
+++ service-runner.sh
@@ -3,8 +3,8 @@
 ARCH=$(uname -m)
 if [ "$ARCH" = "armv7l" ]; then
   echo "Detected architecture: armv7l (ARM). Using armhf-specific override file."
-  docker compose -f docker-compose.yaml -f docker-compose.override.armhf.yaml up
+  docker compose -f docker-compose.yaml -f docker-compose.override.armhf.yaml up -d
 else
   echo "Detected architecture: $ARCH. Using default configuration."
-  docker compose up
+  docker compose up -d
 fi
