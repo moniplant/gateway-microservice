25627fb8a37439913c0354ad53b86a768d1ffda8
mooujarrar <mohyiddine1997@gmail.com>
2025-03-08 13:59:17 +0100

    bug# get the right extension yaml instead of yml for docker compose files

--- service-runner.sh
+++ service-runner.sh
@@ -3,7 +3,7 @@
 ARCH=$(uname -m)
 if [ "$ARCH" = "armv7l" ]; then
   echo "Detected architecture: armv7l (ARM). Using armhf-specific override file."
-  docker compose -f docker-compose.yml -f docker-compose.override.armhf.yml up
+  docker compose -f docker-compose.yaml -f docker-compose.override.armhf.yaml up
 else
   echo "Detected architecture: $ARCH. Using default configuration."
   docker compose up
